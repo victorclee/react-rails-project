23022ea0127dc27bccc50f07243f1a31b3d4157d
victorclee <victorchristopherlee@gmail.com>
2019-03-06 13:59:46 -0600

    Created ten template accounts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times { Account.create!(account_number: "111111111", routing_number: "021000021", bank_address: "201 E Randolph St, Chicago, IL 60602", bank_nickname: "Cloud Gate") }
