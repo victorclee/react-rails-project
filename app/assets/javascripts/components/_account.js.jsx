592d08b7082757f7e12c84a24bc4867ae8611882
victorclee <victorchristopherlee@gmail.com>
2019-03-07 14:59:26 -0600

    make Account a separate entity and move it to a separate Account component

---
+++ app/assets/javascripts/components/_account.js.jsx
@@ -0,0 +1,13 @@
+class Account extends React.Component{
+  
+  render(){
+    return(
+      <div>
+        <h1>{this.props.account.bank_nickname}</h1>
+        <p>{this.props.account.bank_address}</p>
+        <p>{this.props.account.routing_number}</p>
+        <p>{this.props.account.account_number}</p>
+      </div>
+    )      
+  }
+}
--- app/assets/javascripts/components/_all_accounts.js.jsx
+++ app/assets/javascripts/components/_all_accounts.js.jsx
@@ -2,10 +2,7 @@ const AllAccounts = (props) => {
 var accounts = props.accounts.map((account) => {
     return(
    <div key={account.id}>
-    <h1>{account.bank_nickname}</h1>
-    <p>{account.bank_address}</p>
-    <p>{account.routing_number}</p>
-    <p>{account.account_number}</p>
+    <Account account={account}/>
    </div>
     )
   })
