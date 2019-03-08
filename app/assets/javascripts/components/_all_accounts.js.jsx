73a22c9a25e1c34f09ae37dded12e61448f30ea2
victorclee <victorchristopherlee@gmail.com>
2019-03-08 11:59:24 -0600

    handleUpdate() is passed to AllAccounts as a prop, and AllAccounts passes it as a prop to Account

--- app/assets/javascripts/components/_all_accounts.js.jsx
+++ app/assets/javascripts/components/_all_accounts.js.jsx
@@ -2,7 +2,7 @@ const AllAccounts = (props) => {
 var accounts = props.accounts.map((account) => {
     return(
    <div key={account.id}>
-    <Account account={account} handleDelete={props.handleDelete}/>
+    <Account account={account} handleDelete={props.handleDelete} handleUpdate={props.handleUpdate}/>
    </div>
     )
   })
