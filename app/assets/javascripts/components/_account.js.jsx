811e6fd1e6bc35bb45ff4e636de62229b8a5c708
victorclee <victorchristopherlee@gmail.com>
2019-03-08 11:34:09 -0600

    create a state with editable attribute and also to add an Edit button. this.state.editable value will render name and description OR input fields to edit this fruit.

--- app/assets/javascripts/components/_account.js.jsx
+++ app/assets/javascripts/components/_account.js.jsx
@@ -1,12 +1,32 @@
 class Account extends React.Component{
+
+constructor(props){
+    super(props);
+    this.state = {
+      editable: false
+    }
+  }
   
   render(){
+
+    let bank_nickname = this.state.editable ? <input type='text' ref={input => this.bank_nickname = input} defaultValue={this.props.fruit.bank_nickname}/>:<h3>{this.props.fruit.bank_nickname}</h3>
+    let bank_address = this.state.editable ? <input type='text' ref={input => this.bank_address = input} defaultValue={this.props.fruit.bank_address}/>:<p>{this.props.fruit.bank_address}</p>
+    let routing_number = this.state.editable ? <input type='text' ref={input => this.routing_number = input} defaultValue={this.props.fruit.routing_number}/>:<p>{this.props.fruit.routing_number}</p>
+    let account_number = this.state.editable ? <input type='text' ref={input => this.account_number = input} defaultValue={this.props.fruit.account_number}/>:<p>{this.props.fruit.account_number}</p>
+
+
+
+
+
+
+
     return(
       <div>
-        <h1>{this.props.account.bank_nickname}</h1>
-        <p>{this.props.account.bank_address}</p>
-        <p>{this.props.account.routing_number}</p>
-        <p>{this.props.account.account_number}</p>
+        {bank_nickname}
+        {bank_address}
+        {routing_number}
+        {account_number}
+        <button>{this.state.editable? 'Submit' : 'Edit'}</button>
         <button  onClick={() => this.props.handleDelete(this.props.account.id)}>Delete</button>
       </div>
     )      
