2ceb337abbf95c7bae81fc1420eb83b1e64dc01e
victorclee <victorchristopherlee@gmail.com>
2019-03-10 21:13:39 -0500

    Reorganized new account input fields

--- app/assets/javascripts/components/_new_account.js.jsx
+++ app/assets/javascripts/components/_new_account.js.jsx
@@ -4,10 +4,10 @@ const NewAccount = (props) => {
   return(
     <form onSubmit={ (e) => { props.handleFormSubmit(formFields.routing_number.value, formFields.account_number.value, formFields.bank_address.value, formFields.bank_nickname.value); e.target.reset(); e.preventDefault(); }
 }>
+     <input ref={input => formFields.bank_nickname = input} placeholder='Bank nickname' />
+     <input ref={input => formFields.bank_address = input} placeholder='Bank address' />
      <input ref={input => formFields.routing_number = input} placeholder='Routing number'/>
      <input ref={input => formFields.account_number = input} placeholder='Account number' />
-     <input ref={input => formFields.bank_address = input} placeholder='Bank address' />
-     <input ref={input => formFields.bank_nickname = input} placeholder='Bank nickname' />
      <button>Submit</button>
     </form>
   )
